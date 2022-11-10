102a27f831d1a6550c3838858db1d7b22868d4b9
Robert Teets <teetsrobert@gmail.com>
2022-11-09 22:46:41 -0700

    feat: basic styling and autoscroll for messages

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { NextPage } from "next";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
 const Home: NextPage = () => {
     const { data: session } = useSession();
     const [socket, setSocket] = useState<any>();
+    const chatRef = useRef<any>(null);
     const [chat, setChat] = useState([]);
     const [message, setMessage] = useState("");
     const router = useRouter();
@@ -25,6 +26,15 @@ const Home: NextPage = () => {
 
         newSocket.on("getMessages", (response) => {
             setChat(response.messages);
+
+            if (chatRef !== null) {
+                setTimeout(() => {
+                    chatRef.current?.scroll({
+                        top: chatRef.current?.scrollHeight,
+                        behavior: "smooth",
+                    });
+                }, 200);
+            }
         });
 
         return () => {
@@ -53,11 +63,32 @@ const Home: NextPage = () => {
             <div className="flex items-center justify-center h-screen">
                 <div className="w-[500px] bg-neutral-800 bg-opacity-50 backdrop-blur-xl p-5 rounded shadow-2xl">
                     <h1 className="text-4xl text-center">Chat</h1>
-                    <div className="w-full h-[400px] bg-neutral-500 bg-opacity-30 mt-3 overflow-y-scroll scroll-smooth auto">
+                    <div
+                        ref={chatRef}
+                        className="w-full h-[400px] bg-neutral-500 bg-opacity-30 mt-3 overflow-y-scroll p-3 rounded-t flex flex-col gap-3"
+                    >
                         {chat.map((message: any, index) => {
+                            if (message.author === user?.name) {
+                                return (
+                                    <div
+                                        key={index}
+                                        className="bg-cyan-900 text-right px-5 py-2"
+                                    >
+                                        <p className="font-bold text-xl">
+                                            {message.author}
+                                        </p>
+                                        <p>{message.content}</p>
+                                    </div>
+                                );
+                            }
                             return (
-                                <div key={index}>
-                                    <p>{message.author}</p>
+                                <div
+                                    key={index}
+                                    className="bg-purple-900 px-5 py-2"
+                                >
+                                    <p className="font-bold text-xl">
+                                        {message.author}
+                                    </p>
                                     <p>{message.content}</p>
                                 </div>
                             );
@@ -72,11 +103,11 @@ const Home: NextPage = () => {
                             name="content"
                             value={message}
                             onChange={(e) => setMessage(e.target.value)}
-                            className="p-3 bg-neutral-500 bg-opacity-10 flex-[3]"
+                            className="p-3 bg-neutral-500 bg-opacity-10 flex-[3] rounded-bl"
                         />
                         <button
                             type="submit"
-                            className="bg-purple-900 bg-opacity-20 hover:bg-opacity-40 transition-colors flex-1"
+                            className="bg-purple-900 bg-opacity-20 hover:bg-opacity-40 transition-colors flex-1 rounded-br"
                         >
                             Send
                         </button>
