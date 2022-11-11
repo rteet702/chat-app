e8a7e3a55ce29f007861416f837f62271451f2a0
Robert Teets <teetsrobert@gmail.com>
2022-11-10 20:01:27 -0700

    refactor: adjusted clientside code to use new data from prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ const Home: NextPage = () => {
 
     useEffect((): any => {
         const newSocket: Socket = io(
-            "https://chatapp-teets-dev.herokuapp.com/",
-            // "http://localhost:8000/",
+            // "https://chatapp-teets-dev.herokuapp.com/",
+            "http://localhost:8000/",
             { autoConnect: false }
         );
         setSocket(newSocket);
@@ -25,6 +25,7 @@ const Home: NextPage = () => {
         });
 
         newSocket.on("getMessages", (response) => {
+            console.log(response.messages);
             setChat(response.messages);
 
             if (chatRef !== null) {
