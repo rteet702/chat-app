2cd904d42372d7e5c5cb0bd69110a8f04c4e7647
Robert Teets <teetsrobert@gmail.com>
2022-11-12 12:43:26 -0700

    chore: forgot to add index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -128,9 +128,12 @@ const Home: NextPage = () => {
                         </div>
                         {/* sidebar with online users */}
                         <div className="flex-1 mt-3 h-[400px] overflow-y-scroll flex flex-col gap-3">
-                            {onlineUsers.map((user) => {
+                            {onlineUsers.map((user, index) => {
                                 return (
-                                    <div className="rounded px-5 py-2 bg-neutral-500 bg-opacity-30">
+                                    <div
+                                        key={index}
+                                        className="rounded px-5 py-2 bg-neutral-500 bg-opacity-30"
+                                    >
                                         {user.name}
                                     </div>
                                 );
