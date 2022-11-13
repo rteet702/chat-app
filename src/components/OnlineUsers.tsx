ed0eb64dc3bd214f666918eb1e3cf6f79a345773
Robert Teets <teetsrobert@gmail.com>
2022-11-13 09:37:21 -0700

    chore: added basic fallback for onlineusers

--- src/components/OnlineUsers.tsx
+++ src/components/OnlineUsers.tsx
@@ -10,18 +10,25 @@ type User = {
 const OnlineUsers = (props: Props) => {
     const { onlineUsers } = props;
 
+    if (onlineUsers.length > 0) {
+        return (
+            <div className="flex-1 mt-3 h-[400px] overflow-y-scroll flex flex-col gap-3">
+                {onlineUsers.map((user, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="rounded px-5 py-2 bg-neutral-500 bg-opacity-30"
+                        >
+                            {user.name}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
     return (
         <div className="flex-1 mt-3 h-[400px] overflow-y-scroll flex flex-col gap-3">
-            {onlineUsers.map((user, index) => {
-                return (
-                    <div
-                        key={index}
-                        className="rounded px-5 py-2 bg-neutral-500 bg-opacity-30"
-                    >
-                        {user.name}
-                    </div>
-                );
-            })}
+            <p>No users online...</p>
         </div>
     );
 };
